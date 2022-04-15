dc9b75358022a410b3b365b5902e8ff0c3314185
Pablorf10 <rafanet.live@gmail.com>
2022-04-14 21:15:13 -0300

    Personalizando rota

---
+++ src/views/Alunos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Alunos</h1>
+    <div class="cadastro"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.cadastro {
+}
+</style>
